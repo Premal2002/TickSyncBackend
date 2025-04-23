2e82b397af80caff5049d33796b13a9483a63910
PremalWonderbiz <premal.kadam@wonderbiz.in>
2025-04-23 12:37:25 +0530

    updated movie model and added api to sync data from TMDB to out movies table for 5 pages(100 movies)

---
+++ Models/TmdbMovie.cs
@@ -0,0 +1,32 @@
+﻿namespace TickSyncAPI.Models
+{
+    public class TmdbMovie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Overview { get; set; }
+        public string Original_Language { get; set; }
+        public string Poster_Path { get; set; }
+        public string Backdrop_Path { get; set; }
+        public string Release_Date { get; set; }
+        public List<int> Genre_Ids { get; set; }
+        public double Vote_Average { get; set; }
+    }
+
+    public class TmdbResponse
+    {
+        public List<TmdbMovie> results { get; set; }
+    }
+
+    public class TmdbGenre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class TmdbGenreResponse
+    {
+        public List<TmdbGenre> genres { get; set; }
+    }
+
+}
