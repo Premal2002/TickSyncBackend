f865008e09a840d61779239dcd370b772c4f6f73
PremalWonderbiz <premal.kadam@wonderbiz.in>
2025-04-28 14:37:12 +0530

    Reapply "added api to sync languages from tmdb to our table in database"
    This reverts commit c2e418288748f6df639d16e4ee32699bd6f50cd7.

---
+++ Models/Language.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TickSyncAPI.Models;
+
+public partial class Language
+{
+    public int LanguageId { get; set; }
+
+    public string IsoCode { get; set; } = null!;
+
+    public string EnglishName { get; set; } = null!;
+
+    public string? NativeName { get; set; }
+}
