4679a068b1758d0003e7b5edf1f0534339bf5811
PremalWonderbiz <premal.kadam@wonderbiz.in>
2025-05-08 17:39:09 +0530

    added seat lock api with memory caching

---
+++ Models/Dtos/SeatLockInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace TickSyncAPI.Models.Dtos
+{
+    public class SeatLockInfo
+    {
+        public int UserId { get; set; }
+        public DateTime LockedAt { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -59,6 +59,7 @@ builder.Services.AddDbContext<BookingSystemContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
