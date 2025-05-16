7a21c56f1945a0ec7983b80e2b08f17997417933
PremalWonderbiz <premal.kadam@wonderbiz.in>
2025-05-16 16:54:49 +0530

    added authenctication for bookingservice methods and also improved caching and websocket for sending live updates

--- Interfaces/IBookingService.cs
+++ Interfaces/IBookingService.cs
@@ -7,7 +7,7 @@ namespace TickSyncAPI.Interfaces
 {
     public interface IBookingService
     {
-        public Task<List<int>> LockSeatsInCache(SeatLockRequest request);
+        public Task<SeatLockRequest> LockSeatsInCache(SeatLockRequest request);
         public Task<ShowSeatLayoutDto> GetLatestSeatsLayout(int showId);
         public Task<InitiateBookingResponse> InitiateBooking(InitiateBookingRequest request);
         //public Task<ConfirmBookingResponse> ConfirmBooking(ConfirmBookingRequest request);
